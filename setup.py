9de96728bc63cdede19fc25eb5bc0adce0474161
Ayaz Amin <ayaza2004@outlook.com>
2019-04-22 16:04:29 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ from setuptools import setup, find_packages
 with open("requirements.txt", "r") as req:
     req = req.read().splitlines()
 
-with open("README.md", "r") as rm:
-    rm = rm.read()
+path = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(path, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='onnxcv',
@@ -13,7 +15,8 @@ setup(
     author="Ayaz Amin",
     packages=find_packages('onnxcv'),
     install_requires=req,
-    description="An ONNX inference engine for computer vision"
-    long_description=rm,
+    description="An ONNX inference engine for computer vision",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/ChromoBacterium/OnnxCV"
 )
